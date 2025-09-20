3f2e4ebfcb74d65b7525decb35c7e90d8152d143
ARIF <mdarif1738@gmail.com>
2025-09-20 15:09:41 +0530

    some message

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=John
+PERSON2=Raheem
+echo "$PERSON1:: Hi  $PERSON2 , how are you?"
+echo "$PERSON2:: Hello  $PERSON1 , i am fine .How are you ?"
+echo "$PERSON1:: what you learned recently?"
+echo "$PERSON2::  i am larning shell-script $PERSON1"
