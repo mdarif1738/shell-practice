bbf5663badce5d097a5c74ad9562ba68838da93a
ARIF <mdarif1738@gmail.com>
2025-09-20 15:30:42 +0530

    varaiables1

---
+++ variables1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hi  $PERSON2 , how are you?"
+echo "$PERSON2:: Hello  $PERSON1 , i am fine .How are you ?"
+echo "$PERSON1:: what you learned recently?"
+echo "$PERSON2::  i am larning shell-script $PERSON1"
